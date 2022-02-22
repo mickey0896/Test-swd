87ef80e30879a57dd39aa47ef59ee2a11e2b0f14
mickey0896 <mickey_0896@hotmail.com>
2022-02-22 20:14:42 +0700

    update index

--- pages/calendar/index.vue
+++ pages/calendar/index.vue
@@ -1,149 +1,70 @@
 <template>
-
-<div >
-  <v-card class="databox" style="border-radius:0px;float:left;margin-left:10px;">
-    <v-card class="headbox" style="background-color:#32c040;color:#ffff;">
-    <i class="material-icons" style="margin-left:5px; margin-top:8px;">error_outline</i>
-    <span class="assignment" > Upcoming Tasks </span>
-    </v-card>
-  </v-card >
-
-  <v-card class="databox" style="border-radius:0px;float: left;margin-left:30px;">
-    <v-card class="headbox" style="background-color:#32c040;color:#ffff;">
-     <i class="material-icons" style="margin-left:5px; margin-top:8px;">snooze</i>
-    <span class="assignment" > Upcoming Tasks </span>
-    </v-card>
-  </v-card >
-
-  <v-card class="databox" style="border-radius:0px;float: left;margin-left:30px;">
-    <v-card class="headbox" style="background-color:#32c040;color:#ffff;">
-    <i class="material-icons" style="margin-left:5px; margin-top:8px;">sync_alt</i>
-    <span class="assignment" > Upcoming Tasks </span>
-    </v-card>
-  </v-card >
-
-  <v-card class="databox" style="border-radius:0px;float: left;margin-left:30px;">
-    <v-card class="headbox" style="background-color:#32c040;color:#ffff;">
-    <i class="material-icons" style="margin-left:5px; margin-top:8px;">check</i>
-    <span class="assignment" > Upcoming Tasks </span>
-    </v-card>
-  </v-card >
-
-  <v-card class="databox" style="border-radius:0px;float: left;margin-left:30px;">
-    <v-card class="headbox" style="background-color:#32c040;color:#ffff;">
-    <i class="material-icons" style="margin-left:5px; margin-top:8px;">access_alarms</i>
-    <span class="assignment" > Upcoming Tasks </span>
-    </v-card>
-  </v-card >
-
-  <v-card class="databox" style="border-radius:0px;;margin-left:1060px;">
-    <v-card class="headbox" style="background-color:#32c040;color:#ffff;">
-    <i class="material-icons" style="margin-left:5px; margin-top:8px;">access_time</i>
-    <span class="assignment" > Upcoming Tasks </span>
-    </v-card>
-  </v-card >
-<br>
-<v-card >
-  <div style="border-radius:0px;height:40px;
-  background:#32c040; color:#ffff;">
-  <i class="material-icons" style="margin-left:5px; margin-top:8px;" >list_alt</i>
-  <span class="assignment" style="margin-right:15px;"> Assignment of Open Work Orders </span>
-  </div>
-
-  <vs-button
-        circle
-        success
-        style="background-color:#c7f1cb;color:#000000;float:left"
-        :active="active == 3"
-        @click="active = 3"
-        to="management"
-      >
-        <i class="material-icons" >&nbsp;list</i>&nbsp;List&nbsp;
-  </vs-button>
-  <vs-button
-        circle
-        success
-        style="background-color:#c7f1cb;color:#000000;float:left"
-        :active="active == 3"
-        @click="active = 3"
-      >
-         <i class="material-icons" >&nbsp;event_note</i>&nbsp;Calendar&nbsp;
-  </vs-button>
-  <vs-button
-        circle
-        success
-        style="background-color:#c7f1cb;color:#000000;"
-        :active="active == 3"
-        @click="active = 3"
-        
-      >
-        <i class="material-icons" >&nbsp;group</i>&nbsp;User&nbsp;
-  </vs-button>
-        <p style="color:#0C090A;font-size:20px;">Calendar Table</p>
-
-<v-card>
-<vs-table striped>
-    <template #thead>
-      <vs-tr>
-      
-       <vs-th style="padding-right:200px;">
-          Timestamp
-        </vs-th>
-        
-      </vs-tr>
-    </template>
-    <template #tbody>
-      <vs-tr v-for="(tr, index) in users" :key="index" >
-       
-        
-        <vs-td>{{tr.timestamp}}</vs-td>
-       
-      </vs-tr>
-    </template>
-    </vs-table>
-    </v-card>
-</v-card>
-</div>
+  <v-row>
+    <v-col>
+      <v-sheet height="400">
+        <v-calendar
+          ref="calendar"
+          :now="today"
+          :value="today"
+          :events="events"
+          color="primary"
+          type="week"
+        ></v-calendar>
+      </v-sheet>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-import axios  from "axios"
-   export default {
+  export default {
      layout: 'coreLayout',
      auth: false,
-      data()  {
-        return{
-        users: []
-      }
+    data: () => ({
+      today: '2019-01-08',
+      events: [
+        {
+       
+          start: '2019-01-07 09:00',
+          end: '2019-01-07 10:00',
+        },
+        {
+         
+          start: '2019-01-10',
+        },
+        {
+          
+          start: '2019-01-09 12:30',
+          end: '2019-01-09 15:30',
+        },
+      ],
+    }),
+    mounted () {
+      this.$refs.calendar.scrollToTime('08:00')
     },
-    mounted(){
-      const URL = "https://swdapi.ddns.net:8090/data/ttntest"
-      axios
-      .get(URL)
-      .then( res => {
-        console.log(res.data)
-        this.users=res.data.slice(0,100)
-      })
-      },  
-    }
+  }
 </script>
-<style>
-.assignment{
-  color: #fff;
-  text-decoration: none;
-  margin-left:5px;
+
+<style scoped>
+.my-event {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  border-radius: 2px;
+  background-color: #1867c0;
+  color: #ffffff;
+  border: 1px solid #1867c0;
+  font-size: 12px;
+  padding: 3px;
+  cursor: pointer;
+  margin-bottom: 1px;
+  left: 4px;
+  margin-right: 8px;
+  position: relative;
+}
+
+.my-event.with-time {
   position: absolute;
-  top: 8px;
-}
-.databox{
-  
-  height:115px;
-  width:180px;  
-}
-.headbox{
-  
-  height:40px;
-  width:180px; 
+  right: 4px;
+  margin-right: 0px;
 }
 </style>
-
